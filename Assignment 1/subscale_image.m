ca826df9526ee5299e52435974fae0d07b126f0c
Ed Venator <esv@case.edu>
2011-09-07 17:03:56 -0400

    Finished subscaling. Actually added file.

---
+++ Assignment 1/subscale_image.m
@@ -0,0 +1,26 @@
+clear;
+
+%Load image
+img_in = imread('test.jpg','jpg');
+img_in = rgb2gray(img_in);
+[rows, cols] = size(img_in);
+figure(1)
+imshow(img_in)
+truesize;
+title('Original Image')
+
+num_imgs=8;
+
+%Create an output canvas
+img_out = zeros(rows, (num_imgs+1) * cols);
+for i = 0:num_imgs
+    temp = round(double(img_in)*.5^i)*2^i;
+    left_bound = i*cols + 1;
+    right_bound = (i+1)*cols;
+    img_out(:,left_bound:right_bound) = uint8(temp);
+end
+img_out = mat2gray(img_out);
+figure(2)
+imshow(img_out)
+truesize;
+title('Progressively Decreasing Grayscale');

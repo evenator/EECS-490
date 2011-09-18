51371dfd536d9ec9d93127c8cd5c6a828e56c5e1
Ed Venator <esv@case.edu>
2011-09-17 23:03:43 -0400

    Pseudo-coded in comments.

---
+++ Assignment 2/process_image.m
@@ -0,0 +1,16 @@
+%For each image:
+%Load Template Image
+%Load comparison Image
+%Load registration points
+
+%For(1:4)
+%Mark a registration point in the template image
+%Prompt user to click point in comparison image
+
+%Compute warp matrix
+
+%Perform warp and display warped image
+
+%Put both images in greyscale
+
+%Subtract images and display

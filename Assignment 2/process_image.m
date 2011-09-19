79a5e7da5ec53decddb4155bd7bae183069f922d
Ed Venator <esv@case.edu>
2011-09-18 21:51:09 -0400

    Code added to Assignment 2 to setup registration points.

--- Assignment 2/process_image.m
+++ Assignment 2/process_image.m
@@ -1,16 +1,39 @@
 %For each image:
-%Load Template Image
-%Load comparison Image
-%Load registration points
+function processed_image = process_image(template_name, image_name)
+    %Load Template Image
+    template_image = imread(['pics/',template_name,'.bmp']);
+    figure(1)
+    image(template_image);
+    title('Template Image');
+    
+    %Load comparison Image
+    comparison_image = imread(['pics/',image_name,'.jpg']);
+    figure(2)
+    image(comparison_image);
+    title('Comparison Image');
+    
+    %Load registration points
+    reg_points = (xlsread(['point_data/',template_name,'.xls']))';
+    for(reg_point = reg_points)
+        %Mark a registration point in the template image
+        prompt_image = template_image;
+        prompt_image(reg_point)
+        prompt_image(reg_point)=[255,0,0];
+        figure(1)
+        image(prompt_image);
+        title('Click the corresponding point in the other image');
+        %Prompt user to click point in comparison image
+        figure(2)
+        title('Click the corresponding point');
+        point = ginput(1)
+    end
+    
+    %Compute warp matrix
 
-%For(1:4)
-%Mark a registration point in the template image
-%Prompt user to click point in comparison image
+    %Perform warp and display warped image
 
-%Compute warp matrix
+    %Put both images in greyscale
 
-%Perform warp and display warped image
-
-%Put both images in greyscale
-
-%Subtract images and display
+    %Subtract images and display
+    
+end

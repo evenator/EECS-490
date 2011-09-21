5e1d7142e37c1297307287d895d822b912a884f3
Ed Venator <esv@case.edu>
2011-09-21 00:12:06 -0400

    It works. Like actually works. Need to write a main method to do it all and save images, then bed time. Writing the paper in the morning.

--- Assignment 2/process_image.m
+++ Assignment 2/process_image.m
@@ -2,40 +2,43 @@
 function processed_image = process_image(template_name, image_name)
     %Load Template Image
     template_image = imread(['pics/',template_name,'.bmp']);
-    figure(1)
-    image(template_image);
-    title('Template Image');
+    %figure(1)
+    %image(template_image);
+    %title('Template Image');
     template_size = size(template_image);
     
     %Load input Image
     input_image = imread(['pics/',image_name,'.jpg']);
-    figure(2)
-    image(input_image);
-    title('Input Image');
+    %figure(2)
+    %image(input_image);
+    %title('Input Image');
     
     %Select Registration Points
-    %[template_points, image_points] = cpselect(input_image, template_image,'Wait',true);
-    %save('registration_points','template_points','image_points');
-    load('registration_points','template_points','image_points');
+    [template_points, image_points] = cpselect(input_image, template_image,'Wait',true);
+    save([template_name, '->',image_name,' registration_points','template_points','image_points');
+    %load('registration_points','template_points','image_points');
     
     %Create transform object
     transform = cp2tform(template_points, image_points,'affine');
     
     %Transform Image
-    transformed_image = imtransform(input_image, transform, 'bilinear');
-    figure(3)
-    image(transformed_image)
-    title('Transformed Image')
+    transformed_image = imtransform(input_image, transform, 'bilinear','XData',[1 template_size(2)],'YData',[1 template_size(1)]);
+    %figure(3)
+    %image(transformed_image)
+    %title('Transformed Image')
     
     %Put both images in grayscale
     transformed_image = rgb2gray(transformed_image);
-    imshow(transformed_image);
+    %imshow(transformed_image);
     template_image = rgb2gray(template_image);
-    truesize();
-    figure(1)
-    imshow(template_image);
-    truesize();
+    %truesize();
+    %figure(1)
+    %imshow(template_image);
+    %truesize();
     
     %Subtract images and display
+    processed_image = transformed_image - template_image;
+    %figure(4);
+    %imshow(processed_image);
     
 end
